bbe8060938c2d358b4cec1b5f700b6b0661b2c85
wowdevapp <m.eng.abdelhaq@gmail.com>
2021-03-13 18:30:11 +0100

    set up laravel vue project environemment

---
+++ resources/js/app.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="bg-blue-500">
+        <h2>hello lkhooot</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
